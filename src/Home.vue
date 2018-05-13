dbc1dc68cb27ff9eff1519d1487c35f1b1117998
Aurelie Bayre <a.bayre@gmail.com>
2018-05-13 16:31:22 +0200

    feat: improve translate

--- src/Home.vue
+++ src/Home.vue
@@ -1,6 +1,7 @@
 <template>
-  <p @mouseover="getDay">Nous sommes: {{ day }}
-  <button @click="translate">Translate</button>
+  <p>
+    Nous sommes: {{ getDay() }}
+    <button @click="translate">Translate</button>
   </p>
 </template>
 
@@ -8,69 +9,45 @@
 export default {
   data: function() {
     return {
-      dayNum: 1,
-      day: "I dont know...",
+      dayNum: 0,
       isFrench: true
     };
   },
   methods: {
-    getDay() {
-      switch(this.dayNum){
-        case 0:
-          this.day = 'dimanche'
-        case 1:
-          this.day = 'lundi'
-        case 2:
-          this.day = 'mardi'
-        case 3:
-          this.day = 'mercredi'
-        case 4:
-          this.day = 'jeudi'
-        case 5:
-          this.day = 'vendredi'
-        case 6:
-          this.day = 'samedi'
+    translate() {
+      if (this.isFrench){
+        this.isFrench = false
+      }else {
+        this.isFrench = true
       }
     },
-    translate() {
-      if (this.isFrench === true) {
-        this.isFrench = false
-        switch(this.dayNum) {
-          case 0:
-            this.day = 'sunday'
-          case 1:
-            this.day = 'monday'
-          case 2:
-            this.day = 'tuesday'
-          case 3:
-            this.day = 'wednesday'
-          case 4:
-            this.day = 'thursday'
-          case 5:
-            this.day = 'friday'
-          case 6:
-            this.day = 'saturday'
-        }
-      } else {
-        this.isFrench = true
-        switch(this.dayNum) {
-          case 0:
-          this.day = 'dimanche'
+    getDay() {
+      const date = new Date()
+      const dayNum = date.getDay()
+      this.dayNum = dayNum
+      switch(this.dayNum){
+        case 0:
+          this.day = this.isFrench? 'dimanche' : 'sunday'
+          return this.day
         case 1:
-          this.day = 'lundi'
+          this.day = this.isFrench? 'lundi' : 'monday'
+          return this.day
         case 2:
-          this.day = 'mardi'
+          this.day = this.isFrench? 'mardi' : 'tuesday'
+          return this.day
         case 3:
-          this.day = 'mercredi'
+          this.day = this.isFrench? 'mercredi' : 'jeudi'
+          return this.day
         case 4:
-          this.day = 'jeudi'
+          this.day = this.isFrench? 'jeudi' : 'thursday'
+          return this.day
         case 5:
-          this.day = 'vendredi'
+          this.day = this.isFrench? 'vendredi' : 'friday'
+          return this.day
         case 6:
-          this.day = 'samedi'
-        }
+          this.day = this.isFrench? 'samedi' : 'saturday'
+          return this.day
       }
-      
     }
   }
 };
